309deb08a0140e8528515cacd82aeacd98afc81c
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2022-12-03 12:18:27 +0530

    Update PRIMENUM.C

--- PRIMENUM.C
+++ PRIMENUM.C
@@ -2,26 +2,29 @@
 #include<conio.h>
 void main()
 {
-int n,i,flag=0,j,num;
+
+int i=1,j=1,n=0,flag=1,sum;
 clrscr();
-printf("Enter the Number");
-scanf("%d",&n);
 
-	if(n==0 || n==1)
-		flag =1;
-	while(j<=num)
+	printf("Enter the number");
+	scanf("%d",&n);
+
+	for (i=2;i<=n;i++)
 	{
-		for(i=2;i<=n/2;i++)
-			if (n%i == 0)
+		if(n%i==0)
+		{
+			flag=1;
+			for(j=2;j<=i/2;j++)
 			{
-				flag = 1;
-				break;
+				if(i%j == 0)
+				{
+					flag=0;
+					break;
+				}
 			}
-			if (flag == 0)
-			printf(" prime number");
-			else
-			printf("not a prime nuber");
-	 }
-
+			if(flag==1)
+				printf("%d \n",i);
+		}
+	}
 getch();
 }
